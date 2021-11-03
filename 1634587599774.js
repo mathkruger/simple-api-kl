6fa5ce082d44839d19821d1d99d5498c66dcb05f
Matheus da Silva Kruger <matheusk@ciandt.com>
2021-11-03 14:04:18 -0300

    fixing the API and adding script to build it

--- 1634587599774.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const HelloWorld = require('./controllers/HelloWorld');
-
-const routes = express.Router();
-
-routes.post('/', HelloWorld.kasino);
-
-module.exports = routes;
